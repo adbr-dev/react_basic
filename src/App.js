68f949468adaf876d16b0a7fe8050ae831f07414
안드보라 <adbr.dev@gmail.com>
2019-12-09 21:29:13 +0900

    init (components 분리)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Subject from './components/Subject'
+import TOC from './components/TOC'
+import Content from './components/Content'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <Subject title="WEB" sub="world wide web!"></Subject>
+                <TOC></TOC>
+                <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+            </div>
+        );
+    }
 }
-
 export default App;
